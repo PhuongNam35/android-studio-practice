612d5b8d392685eb05de6689fda9d0634f02da25
PhuongNam35 <phuongnamacet@gmail.com>
2025-01-12 22:14:13 +0700

    Data Validation & Error Handling

--- app/src/main/java/com/example/travelblog/LoginActivity.kt
+++ app/src/main/java/com/example/travelblog/LoginActivity.kt
@@ -1,4 +1,64 @@
 package com.example.travelblog
 
-class LoginActivity {
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.travelblog.databinding.ActivityLoginBinding
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textPasswordInput.editText?.addTextChangedListener(createTextWatcher(binding.textPasswordInput))
+        binding.textUsernameLayout.editText?.addTextChangedListener(createTextWatcher(binding.textUsernameLayout))
+
+        binding.loginButton.setOnClickListener() {
+            onLoginClicked()
+        }
+    }
+
+    private fun onLoginClicked() {
+        val username: String = binding.textUsernameLayout.editText?.text.toString()
+        val password: String = binding.textPasswordInput.editText?.text.toString()
+        if (username.isEmpty()) {
+            binding.textUsernameLayout.error = "Username must not be empty"
+        }
+        if (password.isEmpty()) {
+            binding.textPasswordInput.error = "Password must not be empty"
+        }
+        else if (username != "admin") {
+            showErrorDialog()
+        }
+    }
+
+    private fun showErrorDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Login Failed")
+            .setMessage("Username or Password is not correct. Please try again")
+            .setPositiveButton("OK") {dialog, which -> dialog.dismiss()}
+            .show()
+    }
+
+    private fun createTextWatcher(textPasswordInput: TextInputLayout): TextWatcher? {
+        return object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                textPasswordInput.error = null
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        }
+    }
 }
